75e0e4b52f4b9e1eee0fe462a71998d5a79aaad3
SakiPgr <realsaki@hotmail.gr>
2018-12-31 17:56:20 +0200

    It works.It needs refinement and to be a little prettier

--- bitcoinprice.py
+++ bitcoinprice.py
@@ -7,12 +7,11 @@ from openpyxl import Workbook, load_workbook
 
 BITCOIN_PRICE_URL = 'https://api.coinmarketcap.com/v1/ticker/bitcoin/'
 excel_file = 'bitcoinprice.xlsx'
-column_names = ["Katametrisi",
-        "Ημερομηνία",
+column_names = ["Ημερομηνία",
         "Ώρα",
         "Τιμή σε Δολάριο",
         "Τιμή σε Ευρώ",
-        "Timh Dolariou")
+        "Timh Dolariou"]
 
 
 def main():
@@ -22,11 +21,7 @@ def main():
 #  This function will be changed into parts
 def CheckForWorkbook(book):
     if path.isfile(book):
-        wb = load_workbook(book)
-        ws = wb.active
-        ws.append([datetime.datetime.now().strftime("%m-%w-%Y"),
-        datetime.datetime.now().strftime("%H:%M")])
-        return wb
+        return ConsecutiveEntry(book)
     else:
         return FirstEntry()
 
@@ -38,22 +33,53 @@ def FirstEntry():
     return wb
 
 
+def ConsecutiveEntry(book):
+    wb = load_workbook(book)
+    ws = wb.active
+    ws.append([DateAndTime()[0],
+        DateAndTime()[1],
+        RoundNumbers(bitcoin_dollar),
+        RoundNumbers(bitcoin_dollar / dollar_to_euro),
+        GetDollarToEuroConversionRate(accesskey.AccessKey())])
+    return wb
+
+
+def DateAndTime():
+    date_now = datetime.datetime.now().strftime('%m-%w-%Y')
+    time_now = datetime.datetime.now().strftime('%H:%M')
+    return (date_now, time_now)
+
+
 #  The price in Dollar of Bitcoin
 def GetBitcoinPrice(url):
     response = requests.get(BITCOIN_PRICE_URL)
     response_json = response.json()
     bitcoin_price_str = response_json[0]['price_usd']
-    bitcoin_price_round_float = round(float(bitcoin_price_str), 2)
-    return bitcoin_price_round_float
+    return bitcoin_price_str
+
+
+bitcoin_dollar = float(GetBitcoinPrice(BITCOIN_PRICE_URL))
+
+
+def BitcoinPriceInEuro(dollar):
+    price_in_euro = bitcoin_dollar / dollar_to_euro
+    return price_in_euro
+
+
+def RoundNumbers(num):
+    return round(float(num), 2)
 
 
 # The Dollar to Euro conversion rate
 def GetDollarToEuroConversionRate(url):
     response = requests.get(url)
     response_json = response.json()
-    return response_json['quotes']['USDEUR']
+    return float(response_json['quotes']['USDEUR'])
+
+
+dollar_to_euro = GetDollarToEuroConversionRate(accesskey.AccessKey())
 
 
 if __name__ == "__main__":
-    GetDollarToEuroConversionRate(accesskey.AccessKey())
+    main()
 
